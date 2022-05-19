6639d6efc732194c180a8943ee7bc41519107199
Nicolas Einstein <92048614+NicolasRarou@users.noreply.github.com>
2022-05-19 17:48:23 -0300

    Add files via upload

---
+++ Exercicio4_ForLoop.js
@@ -0,0 +1,23 @@
+// contador de 0 a 100
+/*for(let i = 0; i < 100; i++){
+    console.log(i);
+}*/
+
+// repetir um numero
+/*for (let i = 1; i < 100; i++ ){
+    console.log('1');
+}*/
+
+//contador de numeros pares 
+/*for (let i = 1; i < 100; i++){
+    if(i % 2 == 0 ){
+        console.log(i);
+    }
+}*/
+
+// Contador de numeros Impares
+/*for (let i = 1; i < 100; i++){
+    if(i % 2 ==! 0 ){
+        console.log(i);
+    }
+}*/
