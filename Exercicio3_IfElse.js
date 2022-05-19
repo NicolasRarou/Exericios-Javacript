e038d2e304ca674b18c5be9e38dc3080c5686621
Nicolas Einstein <92048614+NicolasRarou@users.noreply.github.com>
2022-05-19 16:57:39 -0300

    Add files via upload

---
+++ Exercicio3_IfElse.js
@@ -0,0 +1,17 @@
+let hora = 18; // Definir Hora 
+
+if(hora> 6 && hora < 12){
+    console.log("Bom dia");
+}
+
+else if(hora >= 12 && hora < 18){
+    console.log("Boa Tarde");
+}
+
+else if (hora > 12 && hora >= 18){
+    console.log ("Boa noite");
+}
+
+else {
+    console.log("Boa Madrugada");
+}
