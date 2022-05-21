bce527bb3199cf9e35b18714f6b993d932446b81
Nicolas Einstein <92048614+NicolasRarou@users.noreply.github.com>
2022-05-21 04:30:37 -0300

    Add files via upload

---
+++ Exercicio7.js
@@ -0,0 +1,57 @@
+// Soma de numeros impares e pares consecutivos
+// usando npm i prompt-sync
+
+let input = require('prompt-sync')();
+
+// impares
+/*console.log("Digite o valor de X: ")
+let x = parseInt(input());
+
+console.log("Digite o valor de Y:")
+let y = parseInt(input());
+
+let soma = 0; // Soma para somar os numeros impares
+let z;
+
+if (x < y){
+for(z= x + 1; z < y; z++){
+    if(z%2 != 0){
+        soma += z;
+    }
+}
+} else {
+    for(z= y + 1; z < x; z++){
+        if(z%2 != 0){
+            soma += z;
+        }
+    }
+}
+
+console.log("A Soma dos numeros impares consecutivos é: " + soma);*/
+
+//pares 
+
+/*console.log("Digite o valor de X: ")
+let x = parseInt(input());
+
+console.log("Digite o valor de Y:")
+let y = parseInt(input());
+
+let soma = 0; // Soma para somar os numeros impares
+let z;
+
+if (x < y){
+for(z= x + 1; z < y; z++){
+    if(z%2 == 0){
+        soma += z;
+    }
+}
+} else {
+    for(z= y + 1; z < x; z++){
+        if(z%2 == 0){
+            soma += z;
+        }
+    }
+}
+
+console.log("A Soma dos numeros pares consecutivos é: " + soma);*/
